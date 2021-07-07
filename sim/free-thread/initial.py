752be528aae95e087d37cd766d6fc70dc6f024ec
luishcc <lh.carnevale@gmail.com>
2021-07-07 19:14:50 +0200

    Testing free-thread simulations

--- sim/free-thread/initial.py
+++ sim/free-thread/initial.py
@@ -3,16 +3,18 @@ from lammps import lammps
 import numpy as np
 
 
-radius = 10
-radius_init = 1.5*radius
-wave_number = 0.55
-wave_length = (2 * np.pi * radius) / wave_number
+radius = 2       # Add as sys.input
+ratio = .7       # Add as sys.input , linear instability when > 1 (Continuum Theory)
+                 # instability above 0.8 (MDPD Simulation)
 
-seed = 38497
+length = radius * ratio * 2 * np.pi
+box_sides = radius * np.cbrt(1.5*ratio*np.pi) + 2
 
-A = -50
+seed = 38497      # Add as sys.input
+
+A = -50           # Add as sys.input
 B = 25
-density = 6.9
+density = 7.0     # Add as sys.input
 
 
 lmp = lammps()
@@ -27,10 +29,10 @@ initial_commands = [
 ]
 
 create_commands = [
-    f"region  mdpd  block -{2*radius} {2*radius} -{2*radius} {2*radius} 0 {wave_length} units box",
+    f"region  mdpd  block -{box_sides} {box_sides} -{box_sides} {box_sides} 0 {length} units box",
     "create_box 1 mdpd",
     f"lattice  fcc {density}",
-    f"region tube cylinder z 0 0 {radius_init} INF INF",
+    f"region tube cylinder z 0 0 {radius} INF INF",
     "create_atoms  1 region tube",
     "mass 1 1.0"
 ]
@@ -41,23 +43,32 @@ pair_commands = [
     f"pair_coeff 1 1 mdpd {A} {B} 18.0 1.0 0.75"
 ]
 
-sim_commands = [
+thermo_commands = [
     "compute             mythermo all temp",
+    "thermo              100",
+    "thermo_modify       temp mythermo",
+    "thermo_modify       flush yes"
+]
+
+sim_commands = [
     f"velocity           all create 1.0 {seed} loop local dist gaussian",
     "comm_modify         vel yes",
-    "fix          mvv    all mvv/dpd 0.65",
+    "fix           mvv   all mvv/dpd 0.65",
     "timestep            0.01"
 ]
 
 lmp.commands_list(initial_commands)
 lmp.commands_list(create_commands)
 lmp.commands_list(pair_commands)
+lmp.commands_list(thermo_commands)
 lmp.commands_list(sim_commands)
 
-lmp.command("run 500")
-lmp.command("minimize 0.0 1.0e-8 10000 100000")
-lmp.command("write_dump all atom dump.atom modify scale no")
+lmp.command("dump mydump all atom 100 thread3.lammpstrj")
+lmp.command("dump_modify mydump scale no")
 
-MPI.Finalize()
+# lmp.command("dump force all custom 100 dump.thread.custom.dump id vx vy vz fx fy fz")
 
-print('testing')
+lmp.command("run 150000")
+
+
+# MPI.Finalize()
