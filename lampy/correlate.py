fade55069d0b3fa1b78bba7ca37b54b62db423b0
luishcc <lh.carnevale@gmail.com>
2021-07-28 18:14:48 +0200

    Testing normalizations on correlation function

--- lampy/correlate.py
+++ lampy/correlate.py
@@ -5,34 +5,28 @@ def correlate(grid, r, dz, rho):
 
     num_phi = grid.get_numphi(r)
 
-    sum = 0
     sumsq = 0
-
     corr = 0
 
     for z in range(grid.num_z - dz):
         for phi in range(num_phi):
             try:
-                # sum += grid.cell[(r, phi, z)].get_density()
                 sumsq += grid.cell[(r, phi, z)].get_density()**2
                 corr += grid.cell[(r, phi, z)].get_density() \
                       * grid.cell[(r, phi, z+dz)].get_density()
-                # print(phi, z, corr)
-
             except KeyError:
                 continue
 
-    nn = (grid.num_z * num_phi)
-
-    # corr /= nn * rho**2
+    nn = (grid.num_z-dz) * num_phi
 
     try:
-        corr /= sumsq
+        corr /= nn * rho**2
+        # corr /= sumsq
+
     except ZeroDivisionError:
         return float('NaN')
 
-    # var = (sumsq - (sum**2/nn))
-    # corr = corr / var
+
 
     return corr - 1
 
@@ -50,6 +44,8 @@ grd = Grid(data, size = float(sys.argv[1]))
 
 
 num = floor(grd.num_z/2)
+# num = grd.num_z
+
 def run(r):
     cor = np.zeros(num)
     for dz in range(num):
@@ -57,7 +53,7 @@ def run(r):
     return cor
 
 res = []
-rrange = 7
+rrange = 5
 for r in range(rrange):
     res.append(run(r))
 
@@ -65,6 +61,32 @@ import matplotlib.pyplot as plt
 
 plt.figure(1)
 for r in range(rrange):
+    if float('Nan') in res[r]:
+        continue
     plt.plot(np.linspace(0,num-1, num), res[r], label=f'R={r}')
 plt.legend()
+
+idr = []
+idz = []
+d = []
+for key, cell in grd.cell.items():
+    idr.append(cell.id[0])
+    idz.append(cell.id[2])
+    d.append(cell.get_density()/cell.nangle)
+
+from scipy.sparse import coo_matrix
+coo = coo_matrix((d, (idr, idz)))
+
+coo = coo.todense().transpose()
+
+
+fig, ax = plt.subplots(1,1)
+im = ax.imshow(coo, extent=[0, 1, 0, 1], aspect=10)
+fig.colorbar(im)
+ax.set_xlabel('Radius')
+ax.set_ylabel('Length')
+ax.yaxis.set_major_locator(plt.NullLocator()) # remove y axis ticks
+ax.xaxis.set_major_locator(plt.NullLocator()) # remove x axis ticks
+plt.show()
+
 plt.show()
