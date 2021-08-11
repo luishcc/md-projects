85259045cdb601c58b10d02dc178d586cd873962
luishcc <lh.carnevale@gmail.com>
2021-08-11 14:40:09 +0200

    testing readline()

--- tests/python/readskip.py
+++ tests/python/readskip.py
@@ -1,11 +1,17 @@
+import time
 file  = 'test.txt'
+
+
 
 
 def skip(f, n):
     for _ in range(n):
         a = f.readline()
-        print(a+'---')
+        print('---'+a)
 
+
+
+t1 = time.time()
 with open(file, 'r') as f:
 
     for line in f:
@@ -13,3 +19,18 @@ with open(file, 'r') as f:
 
         if line.find('skip2') >=0 :
             skip(f, 2)
+
+t2 = time.time()
+
+with open(file, 'r') as f:
+
+    for id, line in enumerate(f):
+        print(line)
+
+        if line.find('skip2') >=0 :
+            skip(f, 1)
+
+t3 = time.time()
+
+print(t2-t1)
+print(t3-t2)
