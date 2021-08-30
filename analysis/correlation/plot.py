185465ffd28282e402de3ebf9dfa3d4a3fee12b5
luishcc <lh.carnevale@gmail.com>
2021-08-30 13:07:28 +0200

    plotting

--- analysis/correlation/plot.py
+++ analysis/correlation/plot.py
@@ -38,6 +38,7 @@ while os.path.isfile(file):
     plt.ylabel(r'$G(r,\delta z)$')
     plt.title(f'R = {R}, Ratio = {ratio}, Snapshot = {snap}')
     plt.ylim(-0.05, 1.1)
+    plt.xlim(0, 110)
     plt.plot([0, data['dz'][-1]], [0, 0], 'k--')
     plt.legend(loc='right')
     plt.savefig(f'{dir_out}/{snap}.png', format='png')
