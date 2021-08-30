dce4074d1d2acd4e98bb42eecf2cec4f727b8e59
luishcc <lh.carnevale@gmail.com>
2021-08-30 19:49:20 +0200

    fourier averages

---
+++ analysis/correlation/fourier.py
@@ -0,0 +1,53 @@
+import sys
+import os
+
+import numpy as np
+from scipy.fft import rfft, rfftfreq
+
+from mdpkg.rwfile import read_dat, Dat
+
+
+R = 10
+ratio = 12
+sim_case = f'R{R}_ratio{ratio}_A50'
+
+path_to_data = os.getcwd()
+dir = '/'.join([path_to_data, sim_case])
+
+dir_out = '/'.join([dir, 'fourier'])
+
+if not os.path.isdir(dir_out):
+    os.mkdir(dir_out)
+
+with open(dir+'/0.dat') as f:
+    labels = f.readline()
+    labels = labels.split()[1:]
+    labels[0] = 'freq'
+    labels = ' '.join(labels)
+
+
+
+snap = 0
+file = dir + f'/{snap}.dat'
+while os.path.isfile(file):
+    print(file)
+    data = read_dat(file)
+
+    num = len(data['dz'])
+    if num % 2 == 0:
+        row = int((num / 2) + 1)
+    else:
+        row = int((num + 1) / 2)
+
+    col = len(data)
+    fourier = np.empty((row, col))
+    fourier[:,:] = np.NaN
+    for i in range(1, col):
+        if not np.any(np.isnan(data[str(i-1)])):
+            fourier[:, i] = rfft(data[str(i-1)])
+    fourier[:,0] = rfftfreq(num)
+
+    fourier_dat = Dat(fourier, labels=labels)
+    fourier_dat.write_file(f'{snap}', dir=dir_out)
+    snap += 1
+    file = dir + f'/{snap}.dat'
---
+++ analysis/correlation/plot_fourier.py
@@ -0,0 +1,48 @@
+import sys
+import os
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from mdpkg.rwfile import read_dat, Dat
+
+
+R = 10
+ratio = 12
+sim_case = f'R{R}_ratio{ratio}_A50/fourier'
+
+path_to_data = os.getcwd()
+dir = '/'.join([path_to_data, sim_case])
+
+dir_out = '/'.join([dir, 'fig'])
+
+if not os.path.isdir(dir_out):
+    os.mkdir(dir_out)
+
+snap = 0
+file = dir + f'/{snap}.dat'
+while os.path.isfile(file):
+    print(file)
+    data = read_dat(file)
+
+    plt.figure(1)
+
+    for i in range(1, len(data)):
+        if not np.any(np.isnan(data[str(i-1)])):
+            plt.plot(data['freq'], data[str(i-1)],
+                     label=f'R={i-1}')
+
+
+
+    plt.xlabel(r'Frequency')
+    plt.ylabel(r'Fourier')
+    plt.title(f'R = {R}, Ratio = {ratio}, Snapshot = {snap}')
+    plt.grid(True)
+    plt.ylim(-0.05, 35)
+    plt.xlim(-0.01, 0.15)
+    plt.plot([0, data['freq'][-1]], [0, 0], 'k--')
+    plt.legend(loc='right')
+    plt.savefig(f'{dir_out}/{snap}.png', format='png')
+    plt.close(1)
+    snap += 1
+    file = dir + f'/{snap}.dat'
