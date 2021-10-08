ef53a3c25b3b3bc97c5588dc7ce6ca817175136c
luishcc <lh.carnevale@gmail.com>
2021-10-08 19:00:33 +0200

    plotting clusters

--- analysis/cluster/plot.py
+++ analysis/cluster/plot.py
@@ -2,28 +2,37 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-
+R = 6
+ratio = 48
 A = -50
-snap = 50
-file = f'/home/luishcc/md-projects/analysis/cluster/R6_ratio48_A{abs(A)}/{snap}.csv'
+snap = 112
+
+file = f'/home/luishcc/md-projects/analysis/cluster/R{R}_ratio{ratio}_A{abs(A)}/{snap}.csv'
 # file = 'a.csv'
 
 
 df = pd.read_csv(file)
 
+print(df.shape)
+
 df.drop(df[df['size'] <= 1].index, inplace=True)
-df.drop(df[df['radius'] > 20].index, inplace=True)
-df.drop(df[df['anisotropy'] > 0.1].index, inplace=True)
+# df.drop(df[df['radius'] > 5].index, inplace=True)
+# df.drop(df[df['size'] > 1000].index, inplace=True)
+df.drop(df[df['anisotropy'] > 0.2].index, inplace=True)
 # df.drop(df[df['asphericity'] > 3].index, inplace=True)
 
+print(df.shape)
 
-# df['radius'].plot()
-df['radius'].plot.kde(bw_method=0.1)
+
+# df['radius'].plot(marker='.', linestyle='none')
+# df['radius'].plot.kde(bw_method=0.1)
 # df['size'].plot.kde(bw_method=0.01)
-# df['radius'].plot.hist(bins=20, alpha=0.5)
-# df['anisotropy'].plot.hist(bins=20, alpha=0.5)
+df['radius'].plot.hist(bins=50, alpha=0.5)
+# df['size'].plot.hist(bins=50, alpha=0.5)
+# df['anisotropy'].plot.hist(bins=50, alpha=0.5)
 # df['asphericity'].plot.hist(bins=20, alpha=0.5)
 # df['acylindricity'].plot.hist(bins=20, alpha=0.5)
+
 
 plt.title(f'Droplet Size Distribution, A={A}, snapshot={snap}')
 # plt.xlim(0, 4)
