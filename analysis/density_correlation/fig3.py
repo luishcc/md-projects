b3b62c337cafaad46844e1295e423df9c71a6e7f
luishcc <lh.carnevale@gmail.com>
2022-11-17 17:34:02 +0100

    length/rho

--- analysis/density_correlation/fig3.py
+++ analysis/density_correlation/fig3.py
@@ -25,6 +25,9 @@ x = np.zeros(len(oh))
 
 x = func(oh)
 
+rho = [7.65, 8.30, 8.95, 9.6, 9.92, 10.24]
+lr = [np.cbrt(1/i) for i in rho]
+invlr = [1/i for i in lr]
 
 # r=5.7
 r=4.8
@@ -33,7 +36,7 @@ r=4.8
 r = [4.8]*6
 ri = [4.8, 4.8, 4.7, 4.7, 4.6, 4.4]
 
-#ri=r
+ri=r
 
 R = 6
 ratio = 48
@@ -44,16 +47,17 @@ oh_data = [0.266,0.321,0.451,0.704,0.901,1.137]
 q = []
 q_var = []
 for iter, a in enumerate(A[:]):
-    file = f'R{R}_ratio{ratio}_A{a}-peak.csv'
+    file = f'peak/R{R}_ratio{ratio}_A{a}-peak.csv'
     with open(file, 'r') as fd:
         fd.readline()
         line = fd.readline().split(',')
         q.append(float(line[0]) * 2 * np.pi * ri[iter])
+        # q.append(1/(float(line[0]) * ri[iter]))
         q_var.append(float(line[1]) * ( 2 * np.pi * ri[iter])**2)
 
 
 
-plt.figure()
+plt.figure(1)
 plt.plot(oh, x, label='Theory', linewidth=1.5, color='k', linestyle='--' )
 
 plt.errorbar(oh_data[:], q, yerr = np.sqrt(q_var), fmt='o',ecolor = 'black',
@@ -65,5 +69,133 @@ plt.xlabel(r'$Oh$')
 plt.legend(loc=0)
 # plt.grid(True)
 
-plt.savefig('fig3.pdf', dpi=dpi, bbox_inches='tight')
+# plt.savefig('fig3.pdf', dpi=dpi, bbox_inches='tight')
+plt.close()
+
+fig, (ax1, ax2) = plt.subplots(1,2, sharey=True)
+fig.set_size_inches(10, 5)
+
+fig.subplots_adjust(wspace=.1)
+
+
+fit = np.polyfit(lr, q, 1)
+scale = (lr[-1] - lr[0]) * 0.1
+a2 = np.linspace(lr[0]-scale, lr[-1]+scale, 100)
+b_fit = [fit[0]*i + fit[1] for i in a2 ]
+
+fit2 = np.polyfit(lr, q, 2)
+b_fit2 = [fit2[0]*i**2 + fit2[1]*i +fit2[2] for i in a2 ]
+
+def ff2(a):
+    return fit2[0]*a**2 + fit2[1]*a +fit2[2]
+
+def ff1(a):
+    return fit[0]*a + fit[1]
+
+def fexp(x,a,b):
+    return a*np.exp(x*b)
+
+def flog(x,a,b):
+    return a*np.log(x) + b
+
+def fpow(x,a,b):
+    return a*x**b
+
+from scipy.optimize import curve_fit
+pars, cov = curve_fit(f=fexp, xdata=lr, ydata=q, p0=[0, 0], bounds=(-np.inf, np.inf))
+pars2, cov2 = curve_fit(f=fpow, xdata=lr, ydata=q, p0=[0, 0], bounds=(-np.inf, np.inf))
+pars3, cov3 = curve_fit(f=flog, xdata=lr, ydata=q, p0=[0, 0], bounds=(-np.inf, np.inf))
+
+stdevs = np.sqrt(np.diag(cov))
+stdevs2 = np.sqrt(np.diag(cov2))
+stdevs3 = np.sqrt(np.diag(cov3))
+
+s1 = 0
+s2 = 0
+s3 = 0
+s4 = 0
+s5 = 0
+for i, j in enumerate(lr):
+    print(i,j)
+    s1 += (ff1(j)-q[i])**2
+    s2 += (ff2(j)-q[i])**2
+    s3 += (fexp(j, *pars)-q[i])**2
+    s4 += (fpow(j, *pars2)-q[i])**2
+    s5 += (fpow(j, *pars3)-q[i])**2
+
+print(s1, s2, s3, s4, s5)
+print(pars, stdevs)
+print(pars2, stdevs2)
+print(pars3, stdevs3)
+print(fit2[0], fit2[1], fit2[2])
+print(fit[0], fit[1])
+
+ax1.set_ylabel(r'$\chi$')
+
+# ax1.plot(lr, q, 'ko', label='Simulation')
+ax1.errorbar(lr, q, yerr = np.sqrt(q_var), fmt='o',ecolor = 'black',
+color='black', label='Simulation', capsize=3, markerfacecolor='none')
+# ax1.plot(a2, [ff1(i) for i in a2], 'k-', label='Linear')
+ax1.plot(a2, [ff2(i) for i in a2], 'k--', label='Fit')
+# ax1.plot(a2, [fexp(i, *pars) for i in a2], 'b-', label='Exponential')
+# ax1.plot(a2, [fpow(i, *pars2) for i in a2], 'b--', label='Power')
+# ax1.plot(a2, [flog(i, *pars3) for i in a2], 'b--', label='Log')
+ax1.set_xlabel(r'$l_{\rho}$')
+ax1.legend()
+ax1.annotate(r'$\chi = -35.9 \ l_{\rho}^2 + 38.5 \ l_{\rho} - 9.7 $', xy=(0.48, 0.40) )
+
+
+
+fit = np.polyfit(invlr, q, 1)
+scale = (invlr[-1] - invlr[0]) * 0.1
+a2 = np.linspace(invlr[0]-scale, invlr[-1]+scale, 100)
+b_fit = [fit[0]*i + fit[1] for i in a2 ]
+
+fit2 = np.polyfit(invlr, q, 2)
+b_fit2 = [fit2[0]*i**2 + fit2[1]*i +fit2[2] for i in a2 ]
+
+pars, cov = curve_fit(f=fexp, xdata=invlr, ydata=q, p0=[0, 0], bounds=(-np.inf, np.inf))
+pars2, cov2 = curve_fit(f=fpow, xdata=invlr, ydata=q, p0=[0, 0], bounds=(-np.inf, np.inf))
+pars3, cov3 = curve_fit(f=flog, xdata=invlr, ydata=q, p0=[0, 0], bounds=(-np.inf, np.inf))
+
+stdevs = np.sqrt(np.diag(cov))
+stdevs2 = np.sqrt(np.diag(cov2))
+stdevs3 = np.sqrt(np.diag(cov3))
+
+s1 = 0
+s2 = 0
+s3 = 0
+s4 = 0
+s5 = 0
+for i, j in enumerate(invlr):
+    print(i,j)
+    s1 += (ff1(j)-q[i])**2
+    s2 += (ff2(j)-q[i])**2
+    s3 += (fexp(j, *pars)-q[i])**2
+    s4 += (fpow(j, *pars2)-q[i])**2
+    s5 += (fpow(j, *pars3)-q[i])**2
+
+print(s1, s2, s3, s4, s5)
+print(pars, stdevs)
+print(pars2, stdevs2)
+print(pars3, stdevs3)
+print()
+print(fit2[0], fit2[1], fit2[2])
+print(fit[0], fit[1])
+
+# ax2.plot(invlr, q, 'ko', label='Simulation')
+ax2.errorbar(invlr, q, yerr = np.sqrt(q_var), fmt='o',ecolor = 'black',
+color='black', label='Simulation', capsize=3, markerfacecolor='none')
+# ax2.plot(a2, [ff1(i) for i in a2], 'k-', label='Linear')
+ax2.plot(a2, [ff2(i) for i in a2], 'k--', label='Fit')
+# ax2.plot(a2, [fexp(i, *pars) for i in a2], 'b-', label='Exponential')
+# ax2.plot(a2, [fpow(i, *pars2) for i in a2], 'b--', label='Power')
+# ax2.plot(a2, [flog(i, *pars3) for i in a2], 'b--', label='Log')
+ax2.set_xlabel(r'$1/l_{\rho}$')
+ax2.annotate(r'$\chi = \displaystyle \frac{-1.5}{l_{\rho}^2} + \frac{5.4}{l_{\rho}} - 4.2 $', xy=(2, 0.40) )
+
+ax2.legend()
+
+fig.tight_layout()
+
 plt.show()
