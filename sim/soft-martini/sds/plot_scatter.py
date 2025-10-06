b99a785ac10f6cb0b740715fe91d66ab577dadd0
luishcc <lh.carnevale@gmail.com>
2025-10-06 18:01:11 +0200

    add exp to scatter plot

--- sim/soft-martini/sds/plot_scatter.py
+++ sim/soft-martini/sds/plot_scatter.py
@@ -57,8 +57,35 @@ ax.set_xlabel(r'q [\AA$^{-1}$]')
 ax.set_ylabel(r'I(q) [.]')
 
 lines, labels = ax.get_legend_handles_labels()
-ax.legend(lines, labels, loc='lower left', frameon=False)
+first_legend = ax.legend(lines, labels, loc='lower left', frameon=False,
+          ncols=2)
+# Add the legend manually to the Axes.
+ax.add_artist(first_legend)
+
+colors = ['brown', 'green']
+linestyles = ['--', '-.']
+for i, con in enumerate([5,20]):
+    file = f'{con}sds.csv'
+    exp_iq = []
+    exp_q = []
+    with open(file, 'r') as fd:
+        fd.readline()
+        for line in fd:
+            exp_iq.append(float(line.split(',')[1]))
+            exp_q.append(float(line.split(',')[0]))
+    zip_lst = zip(exp_q, exp_iq)
+    sort_lst = sorted(zip_lst)
+    tuples = zip(*sort_lst)
+    exp_q, exp_iq  = [list(tuple) for tuple in tuples]
+    exp_iq = np.array(exp_iq)/max(exp_iq)
+    ax.plot(exp_q, exp_iq, label=f'Experimental {con}\%',
+            linestyle=linestyles[i], color=colors[i])
+
+
+lines, labels = ax.get_legend_handles_labels()
+ax.legend(lines[-2:], labels[-2:], loc=[0,0.2], frameon=False,
+          ncols=1)
 
 plt.tight_layout()
-plt.savefig('scatter.pdf', dpi=dpi)
+plt.savefig('scatter_exp.pdf', dpi=dpi)
 plt.show()
